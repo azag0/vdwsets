121c4f10819fbd3badad1727a0d94f6cf3f74b63
Jan Hermann <dev@hermann.in>
2017-05-31 20:44:37 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='vdwsets',
+    version='0.1',
+    description='van der Waals datasets',
+    author='Jan Hermann',
+    author_email='dev@hermann.in',
+    url='https://github.com/azag0/vdwsets',
+    packages=['vdwsets'],
+    package_data={'vdwsets': ['data/*/energies.csv', 'data/*/geoms/*.xyz']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    license='Mozilla Public License 2.0',
+)
