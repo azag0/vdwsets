8e8ae1b3f9f4a03317e6c59a9a93e964ed312a77
Jan Hermann <dev@hermann.in>
2017-05-31 13:15:13 +0200

    upgrade S22

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Computational chemistry datasets
 
 A collection of datasets in computational chemistry. Each set is downloaded from the Internet, processed and stored in a unified format.
+
+- [S22](http://dx.doi.org/10.1039/b600027d)
--- utils/s22/process.py
+++ utils/s22/process.py
@@ -2,7 +2,7 @@
 import sys
 from pathlib import Path
 import geomlib
-import json
+import csv
 from difflib import SequenceMatcher
 
 
@@ -10,7 +10,7 @@ def similarity(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-energies = json.load(sys.stdin)
+energies = list(csv.DictReader(sys.stdin, quotechar="'"))
 prefix = Path(sys.argv[1])
 paths = map(Path, sys.argv[2:])
 
@@ -19,11 +19,14 @@ for path in paths:
     geom = geomlib.readfile(path)
     code, label = path.stem.split('_')
     code = int(code)
+    geom['name'] = label
     frags = geom.get_fragments()
     if not (len(frags) == 2 and geomlib.concat(frags) == geom):
         print(
             f'error: {label} ({code}) was not fragmented correctly',
+            file=sys.stderr
         )
+        sys.exit(1)
     geoms.append({
         'label': label,
         'code': code,
@@ -32,14 +35,27 @@ for path in paths:
     })
 
 geoms.sort(key=lambda x: x['code'])
+geoms.insert(9, geoms.pop(2))
 geom_labels = [g['label'] for g in geoms]
 energy_labels = [row['system name'] for row in energies]
 energies = [energies[l.index(max(l))] for l in [
     [similarity(a, b) for a in energy_labels] for b in geom_labels
 ]]
 
-json.dump(energies, sys.stdout)
-for idx, row in enumerate(geoms):
-    row['complex'].write(prefix/'{:02}-complex-0.xyz'.format(idx+1))
-    for i, fragment in enumerate(row['fragments']):
-        fragment.write(prefix/'{:02}-monomer-{}.xyz'.format(idx+1, i+1))
+for i, row in enumerate(energies):
+    for key, val in row.items():
+        try:
+            row[key] = float(val)
+        except ValueError:
+            pass
+    row['idx'] = i+1
+    row.move_to_end('idx', False)
+
+writer = csv.DictWriter(sys.stdout, energies[0].keys(), quoting=csv.QUOTE_NONNUMERIC)
+writer.writeheader()
+writer.writerows(energies)
+for i, (gs, es) in enumerate(zip(geoms, energies)):
+    name = es['system name'].lower().replace(' ', '-')
+    gs['complex'].write(prefix/f'{i+1:02}_{name}_complex_0.xyz')
+    for j, fragment in enumerate(gs['fragments']):
+        fragment.write(prefix/f'{i+1:02}_{name}_fragment_{j+1}.xyz')
